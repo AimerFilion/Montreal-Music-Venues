22bcc5e45199063e6691d02422695bff7af3df1f
Aimer Filion <aimerfilion@gmail.com>
2022-12-05 00:51:55 -0500

    Add favorite event to user profile

--- client/src/composant/SmallShow.js
+++ client/src/composant/SmallShow.js
@@ -1,11 +1,67 @@
+import styled from "styled-components";
 const SmallShow = ({ show }) => {
   return (
-    <div>
-      <p>{show.title}</p>
-      <p>{show.time}</p>
-      <p>{show.address}</p>
-      <p></p>
-    </div>
+    <>
+      <Table>
+        {/* <tr>
+            <th>DATE</th>
+            <th>EVENT TITLE</th>
+            <th>VENUES</th>
+            <th>TIME</th>
+          </tr> */}
+
+        {/* <tbody> */}
+        <THeader>
+          <tr>
+            <Date>{show.date}</Date>
+            <Title>{show.title}</Title>
+            <Address>{show.address}</Address>
+            <Time>{show.time}</Time>
+          </tr>
+          {/* </tbody> */}
+        </THeader>
+      </Table>
+    </>
   );
 };
+
+const Table = styled.table`
+  table-layout: fixed;
+  width: 90%;
+  border-collapse: collapse;
+  border-bottom: 1px solid grey;
+  color: grey;
+  margin-left: 40px;
+`;
+
+const Date = styled.td`
+  margin-left: 20px;
+  padding: 50px;
+`;
+const Address = styled.td`
+  margin-left: 20px;
+  padding: 50px 50px 50px 70px;
+`;
+const Title = styled.td`
+  padding: 0px;
+`;
+const Time = styled.td`
+  margin-left: 20px;
+  padding: 50px;
+`;
+
+const THeader = styled.thead`
+  th:nth-child(1) {
+    width: 30%;
+  }
+  thead th:nth-child(2) {
+    width: 20%;
+  }
+  thead th:nth-child(3) {
+    width: 15%;
+  }
+  thead th:nth-child(4) {
+    width: 35%;
+  }
+`;
 export default SmallShow;
